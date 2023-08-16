5f17ef837481673ebc2a81c4a49cdcac527467c2
krishnaprasad <krishnaprasad8369@gmail.com>
2023-08-16 12:25:17 +0530

    navbar completed

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './NavBar/Navbar'
+
+function App() {
+ 
+
+  return (
+    <>
+    <Navbar/>
+     
+    </>
+  )
+}
+
+export default App
